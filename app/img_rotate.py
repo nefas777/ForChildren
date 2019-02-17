2f880844ce01901844ce746498dfe3f9bc4d1c79
galinr <ruslan.galinsky@onetick.com>
2019-02-17 14:32:16 +0300

    all

--- app/img_rotate.py
+++ app/img_rotate.py
@@ -4,6 +4,14 @@ import time
 img_list = [r'C:\Users\Nefedov Alex\Desktop\CSapp\app\static\{}.jpg'.format(str(i)) for i in range(1, 9)]
 p = r'C:\Users\Nefedov Alex\Desktop\CSapp\app\static\plot.jpg'
 i=0
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+img_path = os.path.join(current_dir, 'static')
+img_list = [os.path.join(img_path, '%s.jpg' % inx) for inx in range(1, 9)]
+
+p = os.path.join(current_dir, 'static', 'plot.jpg')
+
 while True:
     # if os.path.exists(p):
     #     os.remove(p)
